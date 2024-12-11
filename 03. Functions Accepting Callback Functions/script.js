c9a3a7ba2f1911205904055804a78864122fb96b
ephraim <ephraims.818@gmail.com>
2024-12-11 17:50:12 +0600

    JS A CLOSER LOOK AT FUNCTIONS - 03. Functions Accepting Callback Functions

---
+++ 03. Functions Accepting Callback Functions/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+const transformer = function (str, fn) {
+  console.log(`Transformed string: ${fn(str)}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+const high5 = function () {
+  console.log('👋');
+};
+
+document.body.addEventListener('click', high5);
