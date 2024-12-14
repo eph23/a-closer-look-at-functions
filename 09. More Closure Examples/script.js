68bd825bf74c65ef91fd636d64e1eadded6d0e90
ephraim <ephraims.818@gmail.com>
2024-12-14 15:01:12 +0600

    JS A CLOSER LOOK AT FUNCTIONS - 09. More Closure Examples

---
+++ 09. More Closure Examples/script.js
@@ -0,0 +1,38 @@
+'use strict';
+
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+h();
+f();
+console.dir(f);
+
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are three groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+boardPassengers(180, 3);
